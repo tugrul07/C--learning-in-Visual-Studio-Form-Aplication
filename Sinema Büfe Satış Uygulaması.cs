92ec64ebdb9e3e911f2ae091a31c634a45edd548
tugrul07 <101876837+tugrul07@users.noreply.github.com>
2023-05-03 21:12:45 +0300

    Add files via upload

---
+++ Sinema Büfe Satış Uygulaması.cs
@@ -0,0 +1,15 @@
+namespace Sinema_Büfe_Satış_Uygulaması
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
